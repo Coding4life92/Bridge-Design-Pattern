f8730fcdc80b92af6017bac8ca18d3cab845d67a
Rob Curcio <robby.curcio@gmail.com>
2020-05-18 04:20:49 -0400

    Builder Design Pattern

---
+++ GuitarBridge.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+// Implementor
+class GuitarShop
+{
+public:  
+    virtual void build() = 0;
+};
+
+// Abstraction
+class Guitar 
+{
+public:  
+    Guitar(GuitarShop* guitarshop1, GuitarShop* guitarshop2)
+        : _guitarshop1(guitarshop1),
+        _guitarshop2(guitarshop2) {}
+
+    virtual void manufacture() = 0;
+protected: 
+    GuitarShop* _guitarshop1;
+    GuitarShop* _guitarshop2;
+};
+
+// Concrete implementation 1
+class Produce : public GuitarShop
+{
+public:  
+    virtual void build()
+    {
+        cout << "Produced" << endl;
+    }
+};
+
+// Concrete implementation 2
+class Assmble : public GuitarShop
+{
+public:  
+    virtual void build()
+    {
+        cout << "Assembled" << endl;
+    }
+};
+
+// Refine Abstraction 1
+class Electric : public Guitar
+{
+public:  
+    Electric(GuitarShop* guiarshop1, GuitarShop* guitarshop2)
+        : Guitar(guiarshop1, guitarshop2) {}
+
+    virtual void manufacture()
+    {
+        cout << "Eeletric ";
+        _guitarshop1->build();
+        _guitarshop2->build();
+    }
+};
+
+// Refine Abstraction 2
+class Acoustic : public Guitar
+{
+public:  
+    Acoustic(GuitarShop* guitarshop1, GuitarShop* guitarshop2)
+        : Guitar(guitarshop1, guitarshop2) {}
+
+    virtual void manufacture()
+    {
+        cout << "Acoustic ";
+        _guitarshop1->build();
+        _guitarshop2->build();
+    }
+};
+
+int main()
+{
+    Guitar* guitar1 = new Electric(new Produce(), new Assmble());
+    guitar1->manufacture();
+
+    Guitar* guitar2 = new Acoustic(new Produce(), new Assmble());
+    guitar2->manufacture();
+
+    return 0;
+}
